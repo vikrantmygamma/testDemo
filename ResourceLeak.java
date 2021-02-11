89622de60d6b017de1185d71a2f937e932599f28
Preeti Ghatnekar <52157348+GhatnekarPreeti@users.noreply.github.com>
2021-02-11 09:39:06 +0530

    Update ResourceLeak.java

--- ResourceLeak.java
+++ ResourceLeak.java
@@ -12,6 +12,24 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class ResourceLeak {
+	private int x;
+	private int y;
+	public int getX() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
     LoggerFactory logger = new LoggerFactory();
 	String count;
     public void process() {
