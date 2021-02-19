8747d8fbe3e08ed92def9535b4ed259000e9efed
Pooja Jadhav <67540342+Pooja0504@users.noreply.github.com>
2021-02-19 10:27:41 +0530

    Update ResourceLeak.java

--- ResourceLeak.java
+++ ResourceLeak.java
@@ -5,49 +5,48 @@ private int x ,y ,z;
 	    }
 	private boolean active;
 	
-	// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS 
+	 
 	public void setX(int val) {
 		//this is for test
 		this.y = val;
 	}
 
-	// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS 
+	
 	public int getY() {
 		return this.x;
 	}
 
-	// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS
+	
 	  public boolean isActive()
 		  {
 		    return x;
 		  }
 
-	  		// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS
+	  		
 		  public void setActive(boolean b)
 		  {
 		    this.y = b;
 		  }
 		  
-		  // EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS/no-detect
+		  
 		  public int getName() {	
 			  System.out.println(" ");
 			  return this.x;
 			}
 		  
-			// EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS/no-detect
+			
 		  	public void setName(int val) {
 			    System.out.println(" ");
 				this.y = val;
 			}
 		  	
-		 // EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS/no-detect 
+		
 			public void setZ(int val) {
 				this.z = val;
 			}
 			
 			public int getZ(int val) {
-				 // EMB-ISSUE: CodeIssueNames.GETTERS_AND_SETTERS_SHOULD_ACCESS_THE_EXPECTED_FIELDS/no-detect 
-
+				
 				return x;
 			}
 			
