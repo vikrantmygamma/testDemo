3a2e173094589a980b59737989a3b3a30e83827b
Pooja Jadhav <67540342+Pooja0504@users.noreply.github.com>
2021-02-26 13:41:20 +0530

    Update newChecks.java

--- newChecks.java
+++ newChecks.java
@@ -1,5 +1,74 @@
-public class Tomato implements Externalizable {  
-  public Tomato (String color, int weight) {
-  System.out.println("Hello");  
-  }
+import java.io.*;
+class Car implements Externalizable {
+    static int age;
+    String name;
+    int year;
+ 
+    Car(String n, int y)
+    {
+        this.name = n;
+        this.year = y;
+        age = 10;
+    }
+ 
+    @Override
+    public void writeExternal(ObjectOutput out)
+        throws IOException
+    {
+        out.writeObject(name);
+        out.writeInt(age);
+        out.writeInt(year);
+    }
+ 
+    @Override
+    public void readExternal(ObjectInput in)
+        throws IOException, ClassNotFoundException
+    {
+        name = (String)in.readObject();
+        year = in.readInt();
+        age = in.readInt();
+    }
+ 
+    @Override public String toString()
+    {
+        return ("Name: " + name + "\n"
+                + "Year: " + year + "\n"
+                + "Age: " + age);
+    }
+}
+ 
+public class ExternExample {
+    public static void main(String[] args)
+    {
+        Car car = new Car("Shubham", 1995);
+        Car newcar = null;
+ 
+        // Serialize the car
+        try {
+            FileOutputStream fo
+                = new FileOutputStream("gfg.txt");
+            ObjectOutputStream so
+                = new ObjectOutputStream(fo);
+            so.writeObject(car);
+            so.flush();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+ 
+        // Deserializa the car
+        try {
+            FileInputStream fi
+                = new FileInputStream("gfg.txt");
+            ObjectInputStream si
+                = new ObjectInputStream(fi);
+            newcar = (Car)si.readObject();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+ 
+        System.out.println("The original car is:\n" + car);
+        System.out.println("The new car is:\n" + newcar);
+    }
 }
