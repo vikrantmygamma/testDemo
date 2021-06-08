4330f873a3ab51a67dd973adeb9ba04ed793f248
Pooja Jadhav <67540342+Pooja0504@users.noreply.github.com>
2021-06-08 10:38:37 +0530

    Update ChecksMansi.java

--- ChecksMansi.java
+++ ChecksMansi.java
@@ -1,1 +1,35 @@
 
+public class CheckMansi{
+public  void m(){
+    String ptr = null;
+    try
+    {
+        if (ptr.equals("gfg"))
+            System.out.print("Same");
+        else
+            System.out.print("Not Same");
+    }
+    catch(NullPointerException e)
+    {
+        System.out.print("NullPointerException Caught");
+    }
+}
+  
+  public void m1() {
+File tempDir;
+tempDir = File.createTempFile("", ".");
+tempDir.delete();
+tempDir.mkdir();
+}
+  
+  public void m2() {
+switch (month) {
+case MONDAY:
+case TUESDAY:
+    WEDNESDAY:  
+ system.out.println("it is wednesday");
+  break;
+}
+}
+  
+}
