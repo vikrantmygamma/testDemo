01ccfdc4e17c7225ab683b0c34f08c350c513394
vikrantmygamma <80686843+vikrantmygamma@users.noreply.github.com>
2021-06-17 09:04:04 +0530

    Create issues.java

---
+++ issues.java
@@ -0,0 +1,33 @@
+public void process() {
+        SecureRandom secureRandom = new SecureRandom();
+         System.runFinalizersOnExit(true);
+        try {    
+            PrintWriter out = new PrintWriter("");
+            out.println("the text");
+
+ 
+
+System.out.println("");
+System.out.println("");   
+            out.close();
+            Cipher c1 = Cipher.getInstance("DES");
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+            System.err.println();
+System.err.println();
+
+ 
+
+
+        
+            Cipher c1 = Cipher.getInstance("DES");
+        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+           
+
+ 
+
+
+        }
+
+ 
+
+    }
